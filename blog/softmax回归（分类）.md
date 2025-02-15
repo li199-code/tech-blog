c6ceee806c039b3ef7118f6ba9749bf5d9458aff
Jason Lee <67300063+li199-code@users.noreply.github.com>
2025-02-15 20:09:32 +0800

    katex

--- blog/softmax回归（分类）.md
+++ blog/softmax回归（分类）.md
@@ -14,23 +14,14 @@ date: 2023-03-23 10:20:57
 
 所以，softmax 也是一个单层全连接网络。
 
-```latex
-
-\begin{aligned}
-o*1 &= x_1 w*{11} + x*2 w*{12} + x*3 w*{13} + x*4 w*{14} + b*1,\\
-o_2 &= x_1 w*{21} + x*2 w*{22} + x*3 w*{23} + x*4 w*{24} + b*2,\\
-o_3 &= x_1 w*{31} + x*2 w*{32} + x*3 w*{33} + x*4 w*{34} + b_3.
-\end{aligned}
-```
-
 ## softmax 函数
 
 分类问题的训练，可以用最大化正确分类的概率来表示。因此，要通过 softmax 函数将神经网络的输出$o_i$转化为 0-1 之间的概率。
 
-```latex
+$$
 \begin{equation}
 \hat{\mathbf{y}} = \mathrm{softmax}(\mathbf{o})\quad \text{其中}\quad \hat{y}_j = \frac{\exp(o_j)}{\sum_k \exp(o_k)}
 \end{equation}
-```
+$$
 
 softmax 可以保证输出在 0-1 之间，且 softmax 可导。
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const config: Config = {
   title: 'CodeWithJ',
@@ -56,12 +58,25 @@ const config: Config = {
           
           // 其他配置项...
           onUntruncatedBlogPosts: 'ignore',
+
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         theme: {
           customCss: './src/css/custom.css',
         },
       } satisfies Preset.Options,
     ],
+  ],
+
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
 
   themeConfig: {
